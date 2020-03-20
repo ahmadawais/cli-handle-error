ff557b50a6c0a408c30aa27b0bebc0cd3d3fba24
Ahmad Awais <me@AhmadAwais.com>
2020-03-21 00:57:35 +0500

    📦 NEW: Log symbols

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 /**
  * Node Cli Handle Error.
  */
-const ora = require('ora');
+const sym = require('log-symbols');
 const chalk = require('chalk');
 const red = chalk.bold.red;
 const yellow = chalk.bold.yellow;
-const spinner = ora({ text: '' });
 
 /**
  * @param {String} heading
@@ -17,12 +16,12 @@ module.exports = (heading = `ERROR: `, err, displayError = true, exit = true) =>
 	if (err) {
 		console.log();
 		if (displayError) {
-			spinner.fail(`${red(heading)}`);
-			spinner.fail(`${red(`ERROR →`)} ${err.name}`);
-			spinner.fail(`${red(`REASON →`)} ${err.message}`);
-			spinner.fail(`${red(`ERROR STACK ↓ \n`)} ${err.stack}\n`);
+			console.log(`${sym.fail} ${red(heading)}`);
+			console.log(`${sym.fail} ${red(`ERROR →`)} ${err.name}`);
+			console.log(`${sym.info} ${red(`REASON →`)} ${err.message}`);
+			console.log(`${sym.info} ${red(`ERROR STACK ↓ \n`)} ${err.stack}\n`);
 		} else {
-			spinner.warn(`${yellow(heading)}`);
+			console.log(`${sym.warn} ${yellow(heading)}`);
 		}
 		if (exit) {
 			process.exit(0);
