9c498832c5ac06f44f3d37bb7c80d3308f0ac23d
Ahmad Awais <me@AhmadAwais.com>
2025-01-27 14:56:40 +0500

    👌 IMPROVE: tests

--- test.js
+++ test.js
@@ -1,4 +1,22 @@
+const { Langbase } = require('langbase');
 const handleError = require("./index.js");
 
-const err = new Error([`Something went wrong!`]);
-handleError(`Got an error`, err);
+(async () => {
+	try {
+		const langbase = new Langbase({
+			apiKey: process.env.LANGBASE_API_KEY
+		});
+
+		await langbase.pipe.run({
+			name: 'ai-agent',
+			messages: [
+				{
+					role: 'user',
+					content: 'Who is an AI Engineer?',
+				},
+			],
+		});
+	} catch (err) {
+		handleError('Langbase API Error', err);
+	}
+})();
